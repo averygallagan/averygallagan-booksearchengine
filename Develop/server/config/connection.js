5a0b5db9fbf682d5dfe51d1950da395d847ebd75
Avery <averygallagan@yahoo.com>
2024-08-05 15:50:13 -0400

    updated connections again

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+// Temporarily hardcoding the URI for testing
+const dbURI = 'mongodb+srv://avery:Caveman3@cluster0.xiexsec.mongodb.net/googlebooks?retryWrites=true&w=majority';
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Successfully connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+mongoose.connection.on('error', err => {
+  console.error('MongoDB connection error:', err);
 });
 
 module.exports = mongoose.connection;
