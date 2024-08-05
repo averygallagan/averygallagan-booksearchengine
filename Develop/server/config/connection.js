7551c00ee4af771414c468f9c59434611c1e85c7
Avery <averygallagan@yahoo.com>
2024-08-05 15:38:28 -0400

    connections.js updated

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -5,8 +5,6 @@ const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks'
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  ssl: true, 
-  sslValidate: true,
 });
 
 module.exports = mongoose.connection;
